29e2c6394467016a499687eedafc47277547d4db
ikishansharma0 <aniketjha686@gmail.com>
2025-03-01 23:57:00 +0530

    all patterns

---
+++ date_feb27/triangle_number_patterm.py
@@ -0,0 +1,49 @@
+n = int(input("Enter the size"))
+# for pyramid Triangle
+# for i in range(0,n):
+#     for j in range(0,n-i-1):
+#         print(" ",end="")
+    
+#     for j in range(0,i+1):
+#         print("* ",end="")
+    
+#     print()
+
+# * 
+# * * 
+# * * * 
+# * * * * 
+# * * * * *
+# for i in range(0,n):
+#     for j in range(0,n-1):
+#         print(" ",end="")
+    
+#     for j in range(0,i+1):
+#         print("* ",end="")
+    
+#     print()
+
+# * * * *  
+# * * *   
+# * *    
+# *  
+# for i in range(0,n):
+#     for j in range(0,n-i-1):
+#         print("* ",end="")
+    
+#     for j in range(0,i+1):
+#         print(" ",end="")
+    
+#     print()
+     
+#     *
+#    **
+#   ***
+#  ****
+# for i in range(n,0,-1):
+#     for j in range(0,i):
+#         print(" ", end="")
+#     for j in range(0,n-i):
+#         print("*",end="")
+#     print()
+   
